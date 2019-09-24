e382d06a3f185f8d8dc49209000099b4c5ea7087
Mitch Plants <mitche..plants.2023@student.lhschools.org>
2019-09-24 09:55:33 -0400

    Added a Scanner class and first question.

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Magic8Ball {
     public static void main(String[]args){
+        int x;
+        Scanner Keyboard;
+        Keyboard = new Scanner(System.in);
+        System.out.println("What is your question for The 8 Ball?");
+
+
 
 
 
