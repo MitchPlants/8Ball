2f6e828e9a5b0cced961e2903ca5770e0c4a8d13
Mitch Plants <mitche..plants.2023@student.lhschools.org>
2019-09-24 10:00:44 -0400

    Added a question for a number

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 public class Magic8Ball {
     public static void main(String[]args){
         int x;
+        String FirstAnswer;
         Scanner Keyboard;
         Keyboard = new Scanner(System.in);
         System.out.println("What is your question for The 8 Ball?");
+        FirstAnswer = Keyboard.nextLine();
+        System.out.println("Now a number for the 8 ball");
+        x = Keyboard.nextInt();
+
 
 
 
@@ -42,4 +47,4 @@ public class Magic8Ball {
 
 
         }
-}
+
