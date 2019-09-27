8a319474405d6a0b9a662c68802b756d344d326f
Mitch Plants <mitche..plants.2023@student.lhschools.org>
2019-09-27 09:30:56 -0400

    Added answer for 0-50

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -10,6 +10,18 @@ public class Magic8Ball {
         FirstAnswer = Keyboard.nextLine();
         System.out.println("Now a number for the 8 ball");
         x = Keyboard.nextInt();
+        if(x < 11)
+            System.out.println("Impossible");
+        else if(x<21){
+            System.out.println("Not likely");
+        }
+        else if(x<31){
+            System.out.println("Wouldn't count on it");
+        }
+        else if(x<41){
+            System.out.println("Doesn't look good");
+        }
+        else if(x<51){}
 
 
 
