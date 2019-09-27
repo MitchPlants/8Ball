07cbd290f46b99c43b18c7deb20e82ecfb317595
Mitch Plants <mitche..plants.2023@student.lhschools.org>
2019-09-27 09:53:20 -0400

    Finished the last 5 conditionals 50-100

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -8,20 +8,43 @@ public class Magic8Ball {
         Keyboard = new Scanner(System.in);
         System.out.println("What is your question for The 8 Ball?");
         FirstAnswer = Keyboard.nextLine();
-        System.out.println("Now a number for the 8 ball");
+        System.out.println("Now a number in between 1-100");
         x = Keyboard.nextInt();
-        if(x < 11)
+         if(x>0 && x < 11){
             System.out.println("Impossible");
-        else if(x<21){
+        }
+        else if(x>11 && x<21){
             System.out.println("Not likely");
         }
-        else if(x<31){
+        else if(x>21 && x<31){
             System.out.println("Wouldn't count on it");
         }
-        else if(x<41){
+        else if(x>31 && x<41){
             System.out.println("Doesn't look good");
         }
-        else if(x<51){}
+        else if(x>41 && x<51){
+            System.out.println("The outcome is in your hands");
+        }
+        else if(x>51 && x<61){
+            System.out.println("Looks probable");
+        }
+        else if(x>61 && x<71){
+            System.out.println("The future looks good");
+        }
+        else if(x>71 && x<81){
+            System.out.println("it is almost destiny");
+        }
+        else if(x>81 && x<91){
+            System.out.println("Luck will be on your side");
+        }
+        else if(x>91 && x<101){
+            System.out.println("The gods will make it happen");
+        }
+
+
+
+
+
 
 
 
